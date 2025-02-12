f3f72c47b881485ef735b59908061105c76e51b3
Silvia <git@annhie.de>
2025-02-12 17:10:10 +0100

    main creates a new TextNode object with dummy values and prints its values to console, fixed missing self in constructor and Enum value while creating the object

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    dummy_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(dummy_node)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,25 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url 
+
+    def __eq__(Node1, Node2):
+        if( Node1.text == Node2.text and
+            Node1.text_type == Node2.text_type and
+            Node1.url == Node2.url
+            ):
+            return True 
+    def __repr__(TextNode):
+        return f"TextNode({TextNode.text}, {TextNode.text_type.value}, {TextNode.url})"
+
